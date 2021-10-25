019d688b386cf030f82ee3d24cd23975ec115d73
shubhamSP2198 <84079792+shubhamSP2198@users.noreply.github.com>
2021-10-25 23:16:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Topbar from "./components/Topbar";
 import Main from './components/Main';
+import Footer from "./components/Footer";
 
 const App = ()=>{
   return (
     <div className="App">
         <Topbar/>
         <Main />
+        <Footer/>
     </div>
   );
 }
