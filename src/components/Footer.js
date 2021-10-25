09c4f089f5e2c171b680a5b489f69adbd3e2940f
shubhamSP2198 <84079792+shubhamSP2198@users.noreply.github.com>
2021-10-25 23:18:13 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div>
+                <h4 className="footer-heading">Edyoda</h4>
+                <p>About Us</p>
+                <p>Contact Us</p>
+                <p>Terms of use</p>
+                <p>Privacy Policy</p>
+            </div>
+            <div>
+                <h4 className="footer-heading">Edyoda</h4>
+                <p>Course </p>
+                <p>Learning vides</p>
+                <p>Educator</p>
+                <p>Edyoda Stories</p>
+                <p>University</p>
+            </div>
+            <div>
+                <h4 className="footer-heading">FOR ENTERPRISES</h4>
+                <p>Train your employees</p>
+                <h4 className="footer-heading">QUICK LINKS</h4>
+                <p>Learn and Earn</p>
+                <p>Become a  Learning Enable</p>
+                <p>Tip for Learning Enable</p>
+                <p>Request new Topic</p>
+            </div>
+            <div>
+
+                <h4 className="footer-heading">Connect</h4>
+                <p>2nd Floor #188, Survey No. - 123/1,<br/>
+Incubes Building Next to McDonalds,<br/>
+ITPL Main Rd, Brookefield,<br/>
+Bengaluru, Karnataka-560037,<br/>
+India</p>
+<p>© 2021<br/>
+zekeLabs Technologies Private Limited</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Footer
